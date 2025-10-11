29b4b1bfdf59a5fdc9548ae9db6affbf7137ab5d
tiago <70700766+tiagozip@users.noreply.github.com>
2025-10-11 13:47:27 +0100

    small css

--- public/timeline/js/api.js
+++ public/timeline/js/api.js
@@ -3,6 +3,18 @@ import { authToken } from "./auth.js";
 // this is mostly a foundation to build upon
 // for easier further optimizations
 
+function hash(str) {
+  let h = 2166136261n;
+  for (let i = 0; i < str.length; i++) {
+    h ^= BigInt(str.charCodeAt(i));
+    h *= 16777619n;
+  }
+  const hex = h.toString(16);
+
+  if (hex.length > 32) return hex.slice(0, 32);
+  return hex.padStart(32, "0");
+}
+
 export default (url, options = {}) =>
   new Promise((resolve) => {
     resolve(
@@ -11,6 +23,7 @@ export default (url, options = {}) =>
         headers: {
           ...(options.headers || {}),
           Authorization: `Bearer ${authToken}`,
+          "X-Request-Token": hash(authToken || "public"),
         },
       }).then((r) => r.json())
     );
